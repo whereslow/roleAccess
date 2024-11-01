754c94636c2d38976c351206f5192789db4127e1
whereslow <liruiyang2016@outlook.com>
2024-11-01 09:39:55 +0800

    Version demo

---
+++ control/Register.go
@@ -0,0 +1,28 @@
+package control
+
+import (
+	"CasbinStudio/DAO"
+	"CasbinStudio/config"
+	"CasbinStudio/validate"
+	"github.com/gin-gonic/gin"
+)
+
+func Register(c *gin.Context) {
+	token := c.PostForm("token")
+	newUsername := c.PostForm("new_username")
+	newPassword := c.PostForm("new_password")
+	newRole := c.PostForm("new_role")
+	isAdmin := validate.ValidAdmin(token)
+	if !isAdmin {
+		c.JSON(200, gin.H{"fail": "you are not admin"})
+		return
+	} else {
+		err := DAO.CreateUser(newUsername, newPassword, newRole, config.DB)
+		if err != nil {
+			c.JSON(200, gin.H{"fail": "server error"})
+			return
+		}
+		c.JSON(200, gin.H{"success": "User created"})
+		return
+	}
+}
---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"CasbinStudio/config"
+	"CasbinStudio/control"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	err := config.InitMysql()
+	if err != nil {
+		panic(err)
+	}
+	err = config.InitRedis()
+	if err != nil {
+		panic(err)
+	}
+	r := gin.Default()
+	sso := r.Group("/sso")
+	{
+		sso.POST("/register", control.Register)
+		sso.POST("/login", control.Login)
+		sso.POST("/valid", control.ValidRole)
+		sso.POST("/logout", control.LogOut)
+	}
+
+	err = r.Run("0.0.0.0:8000")
+	if err != nil {
+		panic(err)
+	}
+}
