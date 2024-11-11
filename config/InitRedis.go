02de3457e610c214e74df50ca31b753754a8331c
whereslow <liruiyang2016@outlook.com>
2024-11-11 17:25:27 +0800

    change ENV to Capital letters

--- config/InitRedis.go
+++ config/InitRedis.go
@@ -11,11 +11,11 @@ var RDB *redis2.Client
 
 // InitRedis 创建成功无返回,创建失败返回Redis ping的err
 func InitRedis() error {
-	dbNum, _ := strconv.Atoi(os.Getenv("redis_db"))
-	poolSize, _ := strconv.Atoi(os.Getenv("redis_pool_size"))
+	dbNum, _ := strconv.Atoi(os.Getenv("REDIS_DB"))
+	poolSize, _ := strconv.Atoi(os.Getenv("REDIS_POOL_SIZE"))
 	RDB = redis2.NewClient(&redis2.Options{
-		Addr:     os.Getenv("redis_address"),
-		Password: os.Getenv("redis_password"),
+		Addr:     os.Getenv("REDIS_ADDRESS"),
+		Password: os.Getenv("REDIS_PASSWORD"),
 		DB:       dbNum,
 		PoolSize: poolSize,
 	})
