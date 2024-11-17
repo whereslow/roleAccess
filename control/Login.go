97380c3d4b39633d509f7f1a8c6dda0764eb6cf0
whereslow <liruiyang2016@outlook.com>
2024-11-17 14:53:59 +0800

    add dockerfile to build

--- control/Login.go
+++ control/Login.go
@@ -16,17 +16,9 @@ import (
 // Login 登录,校验账号密码,返回临时token,并存储token和账户在redis,用于校验.如已经登录,则返回失败
 func Login(c *gin.Context) {
 
-	// request json解析及其参数绑定
-	var req struct {
-		Username string `json:"username"`
-		Password string `json:"password"`
-	}
-	err := c.BindJSON(&req)
-	if err != nil {
-		c.JSON(200, gin.H{"flag": "fail", "detail": "request is not standardized", "token": "NULL"})
-	}
-	username := req.Username
-	password := req.Password
+	username := c.PostForm("username")
+	password := c.PostForm("password")
+
 	if username == "" || password == "" {
 		c.JSON(200, gin.H{"flag": "fail", "detail": "username or password is empty", "token": "NULL"})
 		return
